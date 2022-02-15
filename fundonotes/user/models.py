04cb9572912d8c87de876779b2a03eeab49010f4
Luciferkunal16 <Kunalbatham15@gmail.com>
2022-02-15 19:01:08 +0530

    Added UC: JWT Authentication and email part done

--- fundonotes/user/models.py
+++ fundonotes/user/models.py
@@ -4,4 +4,4 @@ from django.db import models
 
 class User(AbstractUser):
     phone_number = models.CharField(max_length=10, unique=False)
-    is_verified = models.BooleanField()
+    is_verified = models.BooleanField(default=False)
