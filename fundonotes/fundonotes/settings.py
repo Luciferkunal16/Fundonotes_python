f84701cacff61204ae6eccf7518e0ce174e4f687
Luciferkunal16 <Kunalbatham15@gmail.com>
2022-02-24 17:38:05 +0530

    code reforfated

--- fundonotes/fundonotes/settings.py
+++ fundonotes/fundonotes/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'user', 'notes',
+'rest_framework',
 
 ]
 AUTH_USER_MODEL = 'user.User'
@@ -124,3 +125,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient"
+        },
+        "KEY_PREFIX": "example"
+    }
+}
