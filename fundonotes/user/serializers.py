17deb5571cfff30209169b507c151cae2c29efb7
Luciferkunal16 <Kunalbatham15@gmail.com>
2022-04-22 14:18:49 +0530

    Changed email service from celery to default

--- fundonotes/user/serializers.py
+++ fundonotes/user/serializers.py
@@ -1,5 +1,3 @@
-
-
 from rest_framework import serializers
 from .models import User
 
@@ -13,7 +11,7 @@ class UserSerializer(serializers.Serializer):
     id = serializers.IntegerField(required=False)
     username = serializers.CharField(allow_blank=False, allow_null=False, required=True)
     email = serializers.CharField(required=False)
-    first_name=serializers.CharField(required=False)
+    first_name = serializers.CharField(required=False)
     last_name = serializers.CharField(required=False)
     password = serializers.CharField(required=True, allow_null=False, allow_blank=False)
     is_verified = serializers.BooleanField(default=False)
@@ -30,14 +28,14 @@ class UserSerializer(serializers.Serializer):
         """
         Update and return an existing `Snippet` instance, given the validated data.
         """
-        instance.id=validated_data.get('id',instance.id)
+        instance.id = validated_data.get('id', instance.id)
         instance.username = validated_data.get('username', instance.username)
         instance.email = validated_data.get('email', instance.email)
-        instance.first_name=validated_data.get('first_name',instance.first_name)
+        instance.first_name = validated_data.get('first_name', instance.first_name)
         instance.last_name = validated_data.get('last_name', instance.last_name)
         instance.password = validated_data.get('password', instance.password)
         instance.is_verified = validated_data.get('is_verified', instance.is_verified)
         instance.phone_number = validated_data.get('phone_number', instance.phone_number)
-        instance.token=validated_data.get('token',instance.token)
+        instance.token = validated_data.get('token', instance.token)
         instance.save()
         return instance
